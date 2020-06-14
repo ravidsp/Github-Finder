dd2cb232bc809369bd4a2f924db20ceddcc1fe23
ravi_sanapala <sravi@ingenious-tec.com>
2020-06-14 19:47:26 +0530

    upf
    update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-export class Navbar extends Component {
+ const Navbar = ({icon,title}) =>  {
 
-    static defaultProps = {
-        title:'Github Finder',
-        icon: 'fab fa-github'
-    }
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon:PropTypes.string.isRequired
-    }
-    render() {
+   
+    
         return (
             <div>
                 <h1>
-        <i className={this.props.icon}></i> {this.props.title}</h1>
+        <i className={icon}></i> {title}</h1>
             </div>
         )
-    }
+    
+}
+Navbar.defaultProps = {
+    title:'Github Finder',
+    icon: 'fab fa-github'
 }
 
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon:PropTypes.string.isRequired
+}
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
- class UserItem extends Component {
+ const UserItem = ({user :{login, avatar_url, html_url}}) =>  {
 
     
-    render() {
-        const {login,avatar_url,html_url}=this.props.user;
+   
+     
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="" className="round-img" style={{width:'60px'}}/>
@@ -18,8 +19,12 @@ import React, { Component } from 'react'
 
 
             </div>
-        )
-    }
+        );
+    
+}
+ 
+UserItem.prototypes = {
+    user:PropTypes.object.isRequired,
 }
 
 export default UserItem
